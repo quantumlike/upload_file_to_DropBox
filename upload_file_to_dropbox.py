e89f62fcd21a95dd7d53c913daf6829fe90a1e62
quantumlike <quantumlike@users.noreply.github.com>
2017-10-16 14:35:51 -0400

    Create upload_file_to_dropbox.py

---
+++ upload_file_to_dropbox.py
@@ -0,0 +1,17 @@
+import dropbox 
+import sys
+# from time import sleep
+
+# This will fail if use an invalid authorization code
+access_token = '<your access token>' # Put the access token obtained from previous file here
+print ("Your access token is:", access_token)
+dbx = dropbox.Dropbox(access_token)
+# sleep(3)
+dbx.users_get_current_account()
+print (dbx.users_get_current_account())
+
+
+file_from = '/home/0012.jpg'
+dest_path = '/test/0012.jpg'
+with open(file_from, 'rb') as f:
+    dbx.files_upload(f.read(), dest_path)
